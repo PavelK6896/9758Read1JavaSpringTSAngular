c6ed0d7b8fc5a3387af84d2d01084014717f3782
PavelK6896 <79208776896@yandex.ru>
2021-08-18 14:21:52 +0300

    update 2

--- src/js/environments/environment.prod.ts
+++ src/js/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: true,
-    URL: 'http://18.156.192.31',
+    // URL: 'http://18.156.192.31',
+    URL: 'https://pavelk-read1.herokuapp.com',
     // URL: 'http://localhost:8080',
+
 
 };
